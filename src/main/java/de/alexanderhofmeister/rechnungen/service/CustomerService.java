a5314f804e2522fab776f73578e2f84b256f9593
Alexander Hofmeister <alexander.hofmeister@ebalance.ch>
2020-11-11 21:44:54 +0100

    Validate duplicate only for new customers

--- src/main/java/de/alexanderhofmeister/rechnungen/service/CustomerService.java
+++ src/main/java/de/alexanderhofmeister/rechnungen/service/CustomerService.java
@@ -7,15 +7,20 @@ import de.alexanderhofmeister.rechnungen.model.Customer;
 
 public class CustomerService extends AbstractEntityService<Customer> {
 
-    public Customer findByCompany(String company, String companyAddition) {
-        return findSingleWithNamedQuery(Customer.NQ_FIND_BY_COMPANY, QueryParameter.with("company", company).and("companyAddition", companyAddition).parameters());
-    }
+  public Customer findByCompany(String company, String companyAddition) {
+    return findSingleWithNamedQuery(Customer.NQ_FIND_BY_COMPANY,
+        QueryParameter.with("company", company).and("companyAddition", companyAddition)
+            .parameters());
+  }
 
-    @Override
-    void validate(Customer entity) throws BusinessException {
-        Customer duplicate = findByCompany(entity.company, entity.companyAddition);
-        if (duplicate != null) {
-            throw new BusinessException("Kunde: " + entity.company + " - " + entity.companyAddition + " existiert bereits!");
-        }
+  @Override
+  void validate(Customer entity) throws BusinessException {
+    if (entity.isNew()) {
+      Customer duplicate = findByCompany(entity.company, entity.companyAddition);
+      if (duplicate != null) {
+        throw new BusinessException(
+            "Kunde: " + entity.company + " - " + entity.companyAddition + " existiert bereits!");
+      }
     }
+  }
 }
